afd9c078b7233f78c228604deedbd61b1ac88731
dmonx <ngurah.natih2017@gmail.com>
2025-07-14 22:38:26 +0800

    feat: tambah perkalian dan pembagian

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ def tambah(a, b):
 def kurang(a, b):
     return a - b
 
+def kali(a, b):
+    return a * b
+
+def bagi(a, b):
+    return a / b if b != 0 else "Tak terdefinisi"
+
+
 def main():
     print("=== Kalkulator CLI ===")
     a = float(input("Masukkan angka pertama: "))
@@ -14,6 +21,10 @@ def main():
         print("Hasil:", tambah(a, b))
     elif operasi == "-":
         print("Hasil:", kurang(a, b))
+    elif operasi == "*":
+        print("Hasil:", kali(a, b))
+    elif operasi == "/":
+        print("Hasil:", bagi(a, b))
     else:
         print("Operasi tidak dikenali")
 
